87021a4ace0af9d856a059dbf482ab5ca451fca8
abel <>
2022-07-14 21:54:30 +0800

    Add table list data

--- backend/app/Http/Controllers/MetoceanController.php
+++ backend/app/Http/Controllers/MetoceanController.php
@@ -34,18 +34,29 @@ class MetoceanController extends Controller
         $result = [];
         $fieldList = DataField::all();
         $keys = DataValue::select()->groupBy('date_time')->orderBy('date_time', 'asc')->get()->toArray();
+        Log::debug($keys);
         $chartData = [];
         foreach ($fieldList as $item ) {
           $fields[] = $item['name'];
           $temp = DataValue::select()->where('field_id', $item['id'])->orderBy('date_time', 'asc')->get()->toArray();
           $chartData[$item['name']] = array_column($temp, 'value');
         }
-       
+        
         $data = [];
         $result['key'] = array_column($keys, 'date_time');
+        
+        foreach ($result['key'] as $index => $e) {
+          $temp = [];
+          $temp['date'] = $e;
+          foreach ($fieldList as $item) {
+            $temp[$item['abbr']] = $chartData[$item['name']][$index];
+          }
+          $data[] = $temp;
+        }
         $result['fields'] = $fields;
         $result['data'] = $data;
         $result['chartData'] = $chartData;
+
 
         return $this->sendResponse($result, "Successfully.");
     }
--- backend/app/Model/DataValue.php
+++ backend/app/Model/DataValue.php
@@ -9,6 +9,6 @@ class DataValue extends Model
     //
     protected $table = 'data_value';
     public $fillable = ['value', 'field_id', 'date_time',];
-    protected $casts = ['id' => 'integer', 'field_id' => 'integer', 'value' => 'float', 'date_time' => 'date'];
+    protected $casts = ['id' => 'integer', 'field_id' => 'integer', 'value' => 'float'];
    
 }
